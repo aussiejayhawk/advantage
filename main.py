670ad14f6fd4f0787b2c497092a88722d9671ce1
jayhawk <aussiejayhawk@gmail.com>
2023-09-24 17:00:15 +1000

    first code attempt name players and roll

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import random
 #get the human players name
 player_name = input("please enter your name ")
 
+#setup a list of player names
 player_names = [player_name, "Bob", "Karen", "Scotty"]
 
+#print the list to test
 print(player_names)
 
-
+#this function rolls a dice, the input determines the number of sides
 def roll(side):
     this_roll = random.randint(1, side)
     return this_roll
 
-
+#this function performs a player turn
 def turn(player):
     dice_no = input("How many dice would you like to roll? ")
 
@@ -29,6 +31,6 @@ def turn(player):
     print(player)
     print(result)
 
-
+#this for loop gives each player a turn
 for p in player_names:
     turn(p)
